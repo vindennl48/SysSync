cf1e998294df65f2ab7253015a2b63e5fc0df99c
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2025-04-19 16:26:48 -0400

    Added arch/install_nvidia.sh

--- arch/install_nvidia.sh
+++ arch/install_nvidia.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-# NVIDIA Driver Install Script for Arch Linux (Supports GRUB and systemd-boot)
+homeDir="$HOME"
+dotfilesDir="${homeDir}/SysSync"
 
-# Check if running as root
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as root"
-    exit 1
-fi
+source "$dotfilesDir/bin/lib"
+
+cprint -p "Installing NVIDIA drivers for Arch!"
 
 # Install NVIDIA packages
-pacman -Sy --noconfirm linux-headers nvidia nvidia-dkms
+sudo pacman -Syu --noconfirm linux-headers nvidia nvidia-dkms
 
 # need to do more testing here.. i think we need to do more with the nvidia-dkms setup..
 
-echo ""
-echo "Installation complete!"
-echo "Please REBOOT your system and verify with:"
-echo "1. nvidia-smi (should show GPU info)"
-echo "2. glxinfo | grep -i \"opengl renderer\" (should show NVIDIA GPU)"
-echo ""
+cprint ""
+cprint -p "Installation complete!"
+cprint "Please REBOOT your system and verify with:"
+cprint "1. nvidia-smi (should show GPU info)"
+cprint "2. glxinfo | grep -i \"opengl renderer\" (should show NVIDIA GPU)"
+cprint ""
